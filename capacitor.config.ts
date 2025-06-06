6bc4f8767fcc3dc7d39d21de74aad88440e7645c
Arcee Luceno <lucenoarcee0@gmail.com>
2025-06-07 02:52:34 +0800

    update appName in capacitor.config.ts to 'GAG RealTime Stock'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'gag-notif',
+  appName: 'GAG RealTime Stock',
   webDir: 'dist'
 };
 
