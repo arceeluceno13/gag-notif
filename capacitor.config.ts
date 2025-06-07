e2882981cbb243e1c0cafc62fd8c839a17f620e1
Arcee Luceno <lucenoarcee0@gmail.com>
2025-06-07 09:47:38 +0800

    update appId in capacitor.config.ts to 'com.gag.app'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.gag.app',
   appName: 'GAG RealTime Stock',
   webDir: 'dist'
 };
